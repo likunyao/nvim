4a05ca617ee60fac1b0b89e775fefe825afdfd48
likunyao <likunyao@bigo.sg>
2022-07-13 19:27:45 +0800

    [fix]update indent

--- lua/basic.lua
+++ lua/basic.lua
@@ -20,8 +20,8 @@ vim.bo.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftround = true
 -- >> << 时移动长度
-vim.o.shiftwidth = 2
-vim.bo.shiftwidth = 2
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
 -- 空格替代tab
 vim.o.expandtab = true
 vim.bo.expandtab = true
