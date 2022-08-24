43f2940e6b2372cd9e0e1f82862656db96066a59
likunyao <nwpulky@gmail.com>
2022-08-24 23:01:14 +0800

    [fix]adapt to coc_0.0.82

--- lua/plugin-config/coc.lua
+++ lua/plugin-config/coc.lua
@@ -1,3 +1,5 @@
+local expr_opts = { noremap = true, silent = true, expr = true }
+
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
 vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
@@ -9,7 +11,19 @@ vim.api.nvim_set_keymap("n", "<leader>h", ":call CocActionAsync('doHover')<CR>",
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 vim.api.nvim_set_keymap("n", "<leader>qf", "<Plug>(coc-fix-current)", {})
 
-vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
-vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+vim.api.nvim_set_keymap("i", "<C-Space>", [[coc#refresh()]], expr_opts)
+
+vim.api.nvim_set_keymap("i",
+    "<TAB>",
+    [[coc#pum#visible() ? coc#pum#next(1) : coc#jumpable() ? "\<c-r>=coc#rpc#request('snippetNext', [])<cr>" : "<TAB>"]],
+    expr_opts)
+
+vim.api.nvim_set_keymap("i",
+    "<S-TAB>",
+    [[coc#pum#visible() ? coc#pum#prev(1) : coc#jumpable() ? "\<c-r>=coc#rpc#request('snippetPrev', [])<cr>" : "<S-TAB>"]],
+    expr_opts)
+
+vim.api.nvim_set_keymap("i",
+    "<CR>",
+    [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+    expr_opts)
