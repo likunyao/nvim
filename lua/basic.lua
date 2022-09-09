2ac9cd43a323d81fdcd5e8d73316e37054a97af5
likunyao <nwpulky@gmail.com>
2022-09-09 20:47:46 +0800

    [fix]enable wrap

--- lua/basic.lua
+++ lua/basic.lua
@@ -41,8 +41,6 @@ vim.o.cmdheight = 1
 -- 当文件被外部程序修改时，自动加载
 vim.o.autoread = true
 vim.bo.autoread = true
--- 禁止折行
-vim.wo.wrap = false
 -- 光标在行首尾时<Left><Right>可以跳到下一行
 vim.o.whichwrap = "<,>,[,]"
 -- 允许隐藏被修改过的buffer
